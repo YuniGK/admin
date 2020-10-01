413a7a83e60b995e1ee682ff6d753be35623da30
YuniGK <1106qwe@hanmail.net>
2020-10-01 21:03:32 +0900

    sample data

--- src/main/java/yuni/kim/study/repository/PartnerRepository.java
+++ src/main/java/yuni/kim/study/repository/PartnerRepository.java
@@ -2,11 +2,15 @@ package yuni.kim.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import yuni.kim.study.model.enitity.Category;
 import yuni.kim.study.model.enitity.Partner;
+
+import java.util.List;
 
 @Repository
 /* Entity의 기본적인 CRUD가 가능하도록 JpaRepository 인터페이스를 사용한다.
 * <어떤 타입 클래스를 사용할 것인지, 기본키의 타입형> */
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
 
+    List<Partner> findByCategory(Category category);
 }
